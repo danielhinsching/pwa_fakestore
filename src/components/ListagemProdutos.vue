f2e791f053df8d3d55e2a42300a5710b88558ef3
danielhinsching <danielsantoshisnsching@gmail.com>
2024-02-28 15:51:31 -0300

    exclusão arquivos desnecessários mais listagem produtos

---
+++ src/components/ListagemProdutos.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const produtos = ref([]);
+
+onMounted(async () => {
+  const response = await axios.get('https://fakestoreapi.com/products');
+  produtos.value = response.data;
+});
+</script>
+
+<template>
+  <div>
+    <h1>Produtos</h1>
+    <div v-for="produto in produtos" :key="produto.id">
+      <h2>{% raw %}{{ produto.title }}{% endraw %}</h2>
+      <p>{% raw %}{{ produto.description }}{% endraw %}</p>
+      <p>{% raw %}{{ produto.price }}{% endraw %}</p>
+      <img :src="produto.image" :alt="produto.title" />
+    </div>
+  </div>
+</template>
